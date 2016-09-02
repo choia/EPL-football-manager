0a134a13c2ef3e1e4e0f1f1c2d5d56735fa1ef20
choia <alex.ji.choi@gmail.com>
2016-09-01 20:17:32 -0400

    Rename class

--- football_manager.py
+++ football_manager.py
@@ -1,20 +1,57 @@
 import sys
+import soccer_club
 # from PyQt5.QtGui import QDoubleValidator, QRegExpValidator
 # from PyQt5.QtCore import QRegExp
-from PyQt5.QtWidgets import QWidget, QApplication, QGridLayout, QGroupBox, QLineEdit, QLabel, QDesktopWidget
+from PyQt5.QtWidgets import QWidget, QApplication, QDesktopWidget, QHBoxLayout, QVBoxLayout, QComboBox, \
+    QGroupBox, QLineEdit, QLabel, QPushButton
 
 
-class Window(QWidget):
+class FootballManager(QWidget):
 
     def __init__(self):
         super().__init__()
         self.init_ui()
 
+    def init_ui(self):
+        '''Initial UI'''
+        self.setWindowTitle('Football Manager')
+        self.resize(600, 550)
+        self.center_window()
 
+        cbox = QComboBox(self)
+        for item_count in range(soccer_club.team_count):
+            cbox.addItem(soccer_club.team_data[item_count]['team_name'])
+
+        self.search_button = QPushButton('Search')
+        self.search_button.clicked.connect(self.run_search)
+
+        self.team_label = QLabel()
+        self.team_label.setText('Test')
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(cbox)
+        hbox.addWidget(self.search_button)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.team_label)
+
+        self.setLayout(hbox)
+
+    def run_search(self):
+        pass
+
+
+
+    def center_window(self):
+        '''Centers the Window on the screen'''
+        window_screen = self.frameGeometry()
+        center_point = QDesktopWidget().availableGeometry().center()
+        window_screen.moveCenter(center_point)
+        self.move(window_screen.topLeft())
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = Window()
-    window.show()
+    fm = FootballManager()
+    fm.show()
     sys.exit(app.exec_())
--- soccer_club.py
+++ soccer_club.py
@@ -25,40 +25,41 @@ for link in soup.find_all("td", {"class": "team left"}):
         team_list.append(link_text)
 
     for src in link.find_all('img'):
-        # print(src['src']) # FOR TESTING
+        # print(src['src']) # TEST
         team_logo_list.append(src['src'])
 
     for a_data in link.find_all('a'):
         urls = a_data.get('href')
         if "squad" in urls:
-            # print(urls) # FOR TESTING
+            # print(urls) # TEST
             team_url_list.append(urls)
 
-# print(team_logo_list) # FOR TESTING
-# print(team_url_list) # FOR TESTING
-# print(team_list) # FOR TESTING
+# print(team_logo_list) # TEST
+# print(team_url_list) # TEST
+# print(team_list) # TEST
 
 number_of_teams = [x for x in range(count) if x % 4 == 0]
-# print(len(number_of_teams)) # FOR TESTING
+# print(len(number_of_teams)) # FOR TEST - Print Number of EPL Teams
 
 for club_data in number_of_teams:
     team_info = {
-        team_list[club_data]: {
+            'team_name': team_list[club_data],
             'coach_name': team_list[club_data + 1],
             'stadium_name': team_list[club_data + 2],
             'city_name': team_list[club_data + 3],
             'club_url': team_url_list[team_count],
             'logo_url': team_logo_list[team_count]
-        }
     }
     team_count += 1
-    print(team_info)
+    # print(team_info) # TEST
     team_data.append(team_info)
 
-# print(team_data) # FOR TESTING
-# print(team_data[1]['Arsenal']['city_name']) # FOR TESTING
-
-
+print(team_data) # TEST
+# print(type(team_data)) # TEST
+''' # TEST - Loops through and prints out Team Name
+for each_team in range(team_count):
+    print(team_data[each_team]['team_name'])
+'''
 
 
 
