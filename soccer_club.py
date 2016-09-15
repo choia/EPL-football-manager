3de9eefdc6ced7d51c8902e9f2813f762ceb38fb
choia <alex.ji.choi@gmail.com>
2016-09-15 14:49:00 -0400

    Lots of functions created for webscraping

--- soccer_club.py
+++ soccer_club.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-# Define a function and use BeautifulSoup and requests to webscrape data from specific url
+# Define a function and use BeautifulSoup & Requests to webscrape data from specific url
 def make_soup(url):
     r = requests.get(url)
     soup_data = BeautifulSoup(r.content, "html.parser")
@@ -19,26 +19,22 @@ team_data = []          # Final list with all of appended club data
 soup = make_soup("http://en.soccerwiki.org/league.php?leagueid=28")
 
 
-# Find all the texts in team left class, appends club name, manager name, stadium, and city to the team info list
+# Find all the texts in team left class, append club name, manager name, stadium, and city to the team info list
 for link in soup.find_all("td", {"class": "team left"}):
     link_text = link.get_text()
     if len(link_text) > 1:
         link_count += 1
         team_info_list.append(link_text)
 
-    # Find all of team logo url(jpg) and appends to the team logo list
+    # Find all of team logo url(jpg) and append to the team logo list
     for jpg_link in link.find_all('img'):
         team_logo_list.append(jpg_link['src'])
 
-    # Find all of club url and appends to the team url list
+    # Find all of club urls and append to the team url list
     for url_link in link.find_all('a'):
         urls = url_link.get('href')
         if "squad" in urls:
             team_url_list.append(urls)
-
-# print(team_logo_list) # TEST
-# print(team_url_list) # TEST
-# print(team_list) # TEST)
 
 
 # Calculate number of teams by link count(total number of texts) divided by 4;
@@ -57,16 +53,7 @@ for club_data in number_of_teams:
             'logo_url': team_logo_list[team_count]
     }
     team_count += 1
-    # print(team_count)
-    # print(team_info) # TEST
     team_data.append(team_info)
-
-# print(team_data) # TEST
-# print(type(team_data)) # TEST
-''' # TEST - Loops through and prints out Team Name
-for each_team in range(team_count):
-    print(team_data[each_team]['team_name'])
-'''
 
 
 
--- squad.py
+++ squad.py
@@ -1,8 +1,59 @@
-from bs4 import BeautifulSoup
-import requests
+import soccer_club
 
-url = 'http://en.soccerwiki.org/squad.php?clubid=1'
-page = requests.get(url)
+url = "http://en.soccerwiki.org/"
 
-soup = BeautifulSoup(page.content)
-print(soup)
+players_data = {}               # Dictionary that holds entire player's data
+player_info = []                # List for player name
+player_temp_info = []           # List for player age and position
+player_count = 0                # Number of players in the club
+club_number = 2                 #PLACEHOLDER UNTIL LOOP ALL OF CLUBS
+
+# Create soup object by appending url + club url from team_data list
+soup = soccer_club.make_soup(url + soccer_club.team_data[club_number]['club_url'])
+
+links = soup.find_all("div", {"class": "InnerBorder"})
+
+# Find all player's name, split last and first names by comma, and rejoin them with first and last name as a string
+for link in links:
+    for player_link in link.find_all('a'):
+        players = player_link.get('href')
+        if "player" in players:
+            player_count += 1 # Count number of players
+            player_lists = player_link.text.split(',')
+            player = ''.join(player_lists[1].lstrip().capitalize() + ' ' + player_lists[0].capitalize())
+            player_info.append({'player_name': player}) # Append each player name to player_info list
+
+    # Find player's age and position and then append to temporary list called player_temp_info
+    for position_link in link.find_all("span", {"class": "actualPos"}):
+        position = position_link.next_sibling.text
+        age = position_link.next_sibling.parent.next_sibling.text
+        player_temp_info.append({'age': age, 'position': position}) # Append each player's age and position to the list
+
+# Merge all player's name, age and position to player_info list
+for count in range(player_count):
+    player_info[count]['age'] = player_temp_info[count]['age']
+    player_info[count]['position'] = player_temp_info[count]['position']
+
+# Define key value pairs and append the player_info to player key
+players_data = {
+    'player': player_info
+    }
+
+# Update the team_data list with players_data dictionary added
+soccer_club.team_data[club_number].update(players_data)
+
+
+# Find club's nickname and year found
+club_links = soup.find_all("div", {"class": "CRcontentBox floatright"})
+for club_link in club_links:
+    nickname = club_link.find_all("td", {"class": "left"})[1].text
+    year_found = club_link.find_all("td", {"class": "left"})[4].text
+
+    # Append nickname and year_found to team_data list
+    soccer_club.team_data[club_number]['nickname'] = nickname
+    soccer_club.team_data[club_number]['year_found'] = year_found
+
+
+print(soccer_club.team_data[club_number])
+#print(soccer_club.team_data)
+print(soccer_club.team_count)
