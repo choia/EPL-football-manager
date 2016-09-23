57fafe121bd328aae616a379bfaf1755241f184a
choia <alex.ji.choi@gmail.com>
2016-09-22 22:12:43 -0400

    Rewriting init_ui function

--- football_manager.py
+++ football_manager.py
@@ -1,9 +1,11 @@
-import sys
 import soccer_club
-# from PyQt5.QtGui import QDoubleValidator, QRegExpValidator
-# from PyQt5.QtCore import QRegExp
+import squad
+import sys
+import urllib.request
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QWidget, QApplication, QDesktopWidget, QHBoxLayout, QVBoxLayout, QComboBox, \
-    QGroupBox, QLineEdit, QLabel, QPushButton
+    QTableWidget, QTableWidgetItem, QLabel, QPushButton, QAbstractItemView, QGroupBox, QLineEdit
 
 
 class FootballManager(QWidget):
@@ -14,33 +16,27 @@ class FootballManager(QWidget):
 
     def init_ui(self):
         '''Initial UI'''
-        self.setWindowTitle('Football Manager')
-        self.resize(600, 550)
-        self.center_window()
+        # Label 'Clubs'
+        club_label = QLabel('Clubs:')
+        club_label.setStyleSheet('font-weight: bold;' 'font-size: 12px;')
+        club_label.setMaximumWidth(40)
 
-        cbox = QComboBox(self)
-        for item_count in range(soccer_club.team_count):
-            cbox.addItem(soccer_club.team_data[item_count]['team_name'])
+        # Combobox for Clubs NEED TO ADDITEMS
+        club_cbox = QComboBox()
+        club_cbox.setStyleSheet('border: 2px solid gray' 'border-radius: 2px;' 'min-width: 5em;')
+        club_cbox.setMinimumHeight(22)
+        club_cbox.setMaximumWidth(230)
 
-        self.search_button = QPushButton('Search')
-        self.search_button.clicked.connect(self.run_search)
-
-        self.team_label = QLabel()
-        self.team_label.setText('Test')
-
-        hbox = QHBoxLayout()
-        hbox.addWidget(cbox)
-        hbox.addWidget(self.search_button)
-
-        vbox = QVBoxLayout()
-        vbox.addWidget(self.team_label)
-
-        self.setLayout(hbox)
+        # Search Button
+        search_btn = QPushButton('Search')
+        search_btn.setStyleSheet('background-color: rgb(255, 255, 240);' 'border: 2px solid black;' 'font: bold 12px;'
+                           'min-width: 5em;' 'padding: 1px;' 'border-style: outset;')
+        pressed_stylesheet = "::pressed{Background-color: orange; border-style: inset;}"
+        search_btn.setStyleSheet(pressed_stylesheet)
+        search_btn.setMaximumWidth(60)
 
     def run_search(self):
         pass
-
-
 
     def center_window(self):
         '''Centers the Window on the screen'''
