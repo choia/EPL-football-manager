3ec4545733ebed9336ce180446a007e1695e47da
choia <alex.ji.choi@gmail.com>
2016-11-16 22:55:15 -0500

    Search function updates

--- football_manager.py
+++ football_manager.py
@@ -24,14 +24,14 @@ class FootballManager(QWidget):
         club_label1.setMaximumWidth(40)
 
         # QCombobox for clubs
-        club_cbox = QComboBox()
-        club_cbox.setStyleSheet('border: 2px solid gray' 'border-radius: 2px;' 'min-width: 5em;')
-        club_cbox.setMinimumHeight(22)
-        club_cbox.setMaximumWidth(230)
+        self.club_cbox = QComboBox()
+        self.club_cbox.setStyleSheet('border: 2px solid gray' 'border-radius: 2px;' 'min-width: 5em;')
+        self.club_cbox.setMinimumHeight(22)
+        self.club_cbox.setMaximumWidth(230)
 
         # Add items from team_data list
         for cbox_item in range(soccer_club.team_count):
-            club_cbox.addItem(soccer_club.team_data[cbox_item]['team_name'])
+            self.club_cbox.addItem(soccer_club.team_data[cbox_item]['team_name'])
 
         # Search Button NEED TO ADD EVENTS WHEN CLICKED
         search_btn = QPushButton('Search')
@@ -45,20 +45,20 @@ class FootballManager(QWidget):
         # Add the label, combobox, and search button to fm_hbox1 layout
         fm_hbox1 = QHBoxLayout()
         fm_hbox1.addWidget(club_label1)
-        fm_hbox1.addWidget(club_cbox)
+        fm_hbox1.addWidget(self.club_cbox)
         fm_hbox1.addWidget(search_btn)
 
         # Club Images NEED TO MOVE/COPY TO SEARCH EVENT FUNCTION
-        image_label = QLabel()
-        image_label.setStyleSheet('background: white;' 'padding-left: 20px;' 'padding-right: 40px;')
+        self.image_label = QLabel()
+        self.image_label.setStyleSheet('background: white;' 'padding-left: 20px;' 'padding-right: 40px;')
         url = soccer_club.team_data[initial_index]['logo_url']
         req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
         data = urllib.request.urlopen(req).read()
         img = QImage()
         img.loadFromData(data)
-        pix_img = img.scaled(60, 60, Qt.KeepAspectRatio)
-        image_label.setPixmap(QPixmap(pix_img))
-        image_label.setAlignment(Qt.AlignCenter)
+        pix_img = img.scaled(66, 66, Qt.KeepAspectRatio)
+        self.image_label.setPixmap(QPixmap(pix_img))
+        self.image_label.setAlignment(Qt.AlignCenter)
 
         # Label Club Info for fm_hbox2 layout
         club_label2 = QLabel('Club Name:\n\nNickname:\n\nFounded:\n\nManager:\n\nLocation:\n\nStadium:')
@@ -66,34 +66,34 @@ class FootballManager(QWidget):
         club_label2.setAlignment(Qt.AlignRight)
 
         # Display club information # DISPLAY INFO WHEN SEARCHED
-        club_label3 = QLabel()
-        club_label3.setText(soccer_club.team_data[initial_index]['team_name'] + "\n\n" +
+        self.club_label3 = QLabel()
+        self.club_label3.setText(soccer_club.team_data[initial_index]['team_name'] + "\n\n" +
                             soccer_club.team_data[initial_index]['nick_name'] + "\n\n" +
                             soccer_club.team_data[initial_index]['year_found'] + "\n\n" +
                             soccer_club.team_data[initial_index]['manager'] + "\n\n" +
                             soccer_club.team_data[initial_index]['location'] + "\n\n" +
                             soccer_club.team_data[initial_index]['stadium'] + "\n\n")
-        club_label3.setStyleSheet('background: white;' 'padding-top: 20px;' 'padding-right: 50px;')
-        club_label3.setAlignment(Qt.AlignLeft)
+        self.club_label3.setStyleSheet('background: white;' 'padding-top: 20px;' 'padding-right: 50px;')
+        self.club_label3.setAlignment(Qt.AlignLeft)
 
         # Add label widgets to fm_hbox2 layout
         fm_hbox2 = QHBoxLayout()
         fm_hbox2.setSpacing(0)
-        fm_hbox2.addWidget(image_label)
+        fm_hbox2.addWidget(self.image_label)
         fm_hbox2.addWidget(club_label2)
-        fm_hbox2.addWidget(club_label3)
+        fm_hbox2.addWidget(self.club_label3)
         fm_hbox2.addStretch(1)
 
         # Add QTableWidget # NEED  TO DISPLAY DATA WHEN SEARCHED
-        table1 = QTableWidget()
-        table1.setStyleSheet('font-size: 11px;' 'alternate-background-color: rgb(176, 224, 208);'
+        self.table1 = QTableWidget()
+        self.table1.setStyleSheet('font-size: 11px;' 'alternate-background-color: rgb(176, 224, 208);'
                              'background-color: rgb(255, 255, 240);')
-        table1.setAlternatingRowColors(True)  # Alternate row colors
-        table1.setColumnCount(3)
-        table1.setEditTriggers(QAbstractItemView.NoEditTriggers)  # Set the rows in Read Only
-        table_vertical = table1.verticalHeader()
+        self.table1.setAlternatingRowColors(True)  # Alternate row colors
+        self.table1.setColumnCount(3)
+        self.table1.setEditTriggers(QAbstractItemView.NoEditTriggers)  # Set the rows in Read Only
+        table_vertical = self.table1.verticalHeader()
         table_vertical.setVisible(False)  # Disable vertical column numbers
-        table1_horizontal = table1.horizontalHeader()
+        table1_horizontal = self.table1.horizontalHeader()
         table1_horizontal.setStretchLastSection(True)  # Stretch and close the gap
 
         header_name = [
@@ -103,28 +103,28 @@ class FootballManager(QWidget):
         ]
 
         # Set number_of_rows for the table
-        for number_of_row in range(squad.player_count):
-            table1.setRowHeight(number_of_row, 1)
+        #for number_of_row in range(len(soccer_club.team_data[initial_index]['players'])):
+        #    self.table1.setRowHeight(number_of_row, 1)
 
-        # Print(len(soccer_club.team_data[initial_index]['players']))
+        #print(len(soccer_club.team_data[initial_index]['players']))
 
         # Set rows on table by number of data
-        table1.setRowCount(len(soccer_club.team_data[initial_index]['players']))
+        self.table1.setRowCount(len(soccer_club.team_data[initial_index]['players']))
 
         # Adding player data into table
         for number_of_player, key in enumerate(soccer_club.team_data[initial_index]['players'].keys()):
             for number_of_data, item in enumerate(soccer_club.team_data[initial_index]['players'][key]):
-                table1.setItem(number_of_player, number_of_data, QTableWidgetItem(item))
+                self.table1.setItem(number_of_player, number_of_data, QTableWidgetItem(item))
 
-        table1.setHorizontalHeaderLabels(header_name)  # Set column name
+                self.table1.setHorizontalHeaderLabels(header_name)  # Set column name
         stylesheet = "::section{Background-color:rgb(248,248,255)}"
-        table1.horizontalHeader().setStyleSheet(stylesheet)
-        table1.setColumnWidth(0, 160)  # Set column width
-        table1.resizeRowsToContents()
+        self.table1.horizontalHeader().setStyleSheet(stylesheet)
+        self.table1.setColumnWidth(0, 160)  # Set column width
+        self.table1.resizeRowsToContents()
 
         # fm_hbox3 layout
         fm_hbox3 = QHBoxLayout()
-        fm_hbox3.addWidget(table1)
+        fm_hbox3.addWidget(self.table1)
 
         # Overall layout
         fm_vbox = QVBoxLayout()
@@ -136,13 +136,42 @@ class FootballManager(QWidget):
         self.setStyleSheet('background: white;')
         #self.setGeometry(300, 300, 400, 600)
         self.center_window()
-        self.setFixedSize(400, 600)  # Disable resizing widget window
+        self.setFixedSize(380, 600)  # Disable resizing widget window
         self.show()
 
 
     def run_search(self):
-        pass
+        #print(self.club_cbox.currentIndex())
+        index = self.club_cbox.currentIndex()
 
+        # Update the logo image
+        url = soccer_club.team_data[index]['logo_url']
+        req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        data = urllib.request.urlopen(req).read()
+        img = QImage()
+        img.loadFromData(data)
+        pix_img = img.scaled(66, 66, Qt.KeepAspectRatio)
+        self.image_label.setPixmap(QPixmap(pix_img))
+
+        # Update the Club information
+        self.club_label3.setText(soccer_club.team_data[index]['team_name'] + "\n\n" +
+                                 soccer_club.team_data[index]['nick_name'] + "\n\n" +
+                                 soccer_club.team_data[index]['year_found'] + "\n\n" +
+                                 soccer_club.team_data[index]['manager'] + "\n\n" +
+                                 soccer_club.team_data[index]['location'] + "\n\n" +
+                                 soccer_club.team_data[index]['stadium'] + "\n\n")
+
+        # Populate the table
+        #for number_of_row in range(len(soccer_club.team_data[index]['players'])):
+        #    self.table1.setRowHeight(number_of_row, 1)
+
+        self.table1.setRowCount(len(soccer_club.team_data[index]['players']))
+
+        for number_of_player, key in enumerate(soccer_club.team_data[index]['players'].keys()):
+            for number_of_data, item in enumerate(soccer_club.team_data[index]['players'][key]):
+                self.table1.setItem(number_of_player, number_of_data, QTableWidgetItem(item))
+
+    # Center Main Window
     def center_window(self):
         '''Centers the Window on the screen'''
         window_screen = self.frameGeometry()
--- squad.py
+++ squad.py
@@ -1,56 +1,57 @@
 import soccer_club
 
 url = "http://en.soccerwiki.org/"
+players_data = {}                   # Dictionary that holds entire player's data
 
-players_data = {}               # Dictionary that holds entire player's data
-player_info = {}                # Dictionary for player data
-player_count = 0                # Number of players in the club
-count = 0                       # Counter used to append age and position to player_info
+# Looping entire EPL team
+for club_ID in range(soccer_club.team_count):
 
-# Create soup object by appending url + club url from team_data list
-soup = soccer_club.make_soup(url + soccer_club.team_data[0]['club_url'])
+    player_info = {}                # Dictionary for player data
+    player_count = 0                # Number of players in the club
+    count = 0                       # Counter used to append age and position to player_info
 
-# Find div element with class name Innerborder that contains data can be webscraped
-links = soup.find_all("div", {"class": "InnerBorder"})
+    # Create soup object by appending url + club url from team_data list
+    soup = soccer_club.make_soup(url + soccer_club.team_data[club_ID]['club_url'])
 
-# Find all player's name, split last and first names by comma, and rejoin them with first and last name as a string
-for link in links:
-    for player_link in link.find_all('a'):
-        players = player_link.get('href')
-        if "player" in players:
-            player_lists = player_link.text.split(',')
-            player = ''.join(player_lists[1].lstrip().capitalize() + ' ' + player_lists[0].capitalize())
-            player_name = player_count
-            player_info.update({player_name: [player]})
-            player_count += 1
+    # Find div element with class name Innerborder that contains data can be webscraped
+    links = soup.find_all("div", {"class": "InnerBorder"})
 
-    # Find player's age and position and then append to player_info dictionary
-    for position_link in link.find_all("span", {"class": "actualPos"}):
-        position = position_link.next_sibling.text
-        age = position_link.next_sibling.parent.next_sibling.text
-        player_info[count].append(age)
-        player_info[count].append(position)
-        count += 1
+    # Find all player's name, split last and first names by comma, and rejoin them with first and last name as a string
+    for link in links:
+        for player_link in link.find_all('a'):
+            players = player_link.get('href')
+            if "player" in players:
+                player_lists = player_link.text.split(',')
+                player = ''.join(player_lists[1].lstrip().capitalize() + ' ' + player_lists[0].capitalize())
+                player_name = player_count
+                player_info.update({player_name: [player]})
+                player_count += 1
 
-#print(player_info)
+        # Find player's age and position and then append to player_info dictionary
+        for position_link in link.find_all("span", {"class": "actualPos"}):
+            position = position_link.next_sibling.text
+            age = position_link.next_sibling.parent.next_sibling.text
+            player_info[count].append(age)
+            player_info[count].append(position)
+            count += 1
 
-# Define key value pairs and append the player_info to player key
-players_data = {
-    'players': player_info
-}
+    # Define key value pairs and append the player_info to player key
+    players_data = {
+        'players': player_info
+    }
 
-# Update the team_data list with players_data dictionary added
-soccer_club.team_data[0].update(players_data)
+    # Update the team_data list with players_data dictionary added
+    soccer_club.team_data[club_ID].update(players_data)
 
-# Find club's nickname and year found
-club_links = soup.find_all("div", {"class": "CRcontentBox floatright"})
-for club_link in club_links:
-    nickname = club_link.find_all("td", {"class": "left"})[1].text
-    year_found = club_link.find_all("td", {"class": "left"})[4].text
+    # Find club's nickname and year found
+    club_links = soup.find_all("div", {"class": "CRcontentBox floatright"})
+    for club_link in club_links:
+        nickname = club_link.find_all("td", {"class": "left"})[1].text
+        year_found = club_link.find_all("td", {"class": "left"})[4].text
 
-    # Append nickname and year_found to team_data list
-    soccer_club.team_data[0]['nick_name'] = nickname
-    soccer_club.team_data[0]['year_found'] = year_found
+        # Append nickname and year_found to team_data list
+        soccer_club.team_data[club_ID]['nick_name'] = nickname
+        soccer_club.team_data[club_ID]['year_found'] = year_found
 
 #print(soccer_club.team_data) # TEST
 #print(players_data) # TEST
